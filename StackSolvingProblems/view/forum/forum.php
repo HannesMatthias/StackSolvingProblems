f12b37445e36b58f2e2d1ffb74ecc86c0502b893
[HannesMatthias] <[mh14270@gmail.com]>
2018-05-17 14:08:28 +0200

    menu hochgeladen

---
+++ StackSolvingProblems/view/forum/forum.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+
+<html>
+
+    <head>
+        <title>Frage die Community</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <link href="../menu/menu.css" type="text/css" rel="stylesheet" /> 
+    </head>
+
+    <body>
+        <?php include_once "../menu/menu.php" ?>
+
+    </body>
+
+
+</html>
--- StackSolvingProblems/view/menu/menu.php
+++ StackSolvingProblems/view/menu/menu.php
@@ -1,11 +1,15 @@
 <nav id="menu">
-    
     <ul>
-        <li><a href="index.php">Logo Bild</a></li>
+        <li><a href="index.php"><img id="logo" src="../images/logo.png" /></a></li>
+        <li>
+            <form>
+                <input type="search" id="searchbar" placeholder="Suche nach Fragen" />
+                <input type="submit" id="searchButton" value="Suchen" />
+
+            </form>
+        </li>
         <li><a href="#">Frage stellen</a></li>
-        <li><a href="#">Probleme lösen</a> </li>
-        <li><a href="#"></a></li>
-        
+        <li><a href="#">Probleme lösen</a> </li>        
         <li><a href="#">An Projekten teilnehmen</a></li>
     </ul>
 </nav>
