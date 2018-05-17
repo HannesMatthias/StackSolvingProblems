cdcb116e7cd5d1b7e83f5a93e9644770adfd4b2f
manuel_ceschini <Manuel.Ceschini@gmail.com>
2018-05-17 14:30:38 +0200

    menu tpl and css
    Signed-off-by: manuel_ceschini <Manuel.Ceschini@gmail.com>

--- StackSolvingProblems/view/menu.tpl.php
+++ StackSolvingProblems/view/menu.tpl.php
@@ -2,9 +2,12 @@
 <html>
 <head>
     <title>PIM</title>
+    <link rel="stylesheet" type="text/css" href="menu.tpl.css">
 </head>
 <body>
-    <header>
+    <header class=headerMenu>
+    <a href="" class=usLayou> login </a>
+    <a href="" class=usLayou> register </a>
 
     </header>
 </body>
