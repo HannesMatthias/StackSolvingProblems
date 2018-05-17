5795cc371a6ffe3661e9ceef888e62b915777d19
manuel_ceschini <Manuel.Ceschini@gmail.com>
2018-05-17 14:13:43 +0200

    struktur änderung

--- StackSolvingProblems/index.php
+++ StackSolvingProblems/index.php
@@ -6,4 +6,4 @@
 
         $controller->run($aktion);
     }
-?>
+?>                   
---
+++ StackSolvingProblems/view/menu.tpl.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PIM</title>
+</head>
+<body>
+    <header>
+
+    </header>
+</body>
+</html>  
