3ce02cf8952ef2e1ac54326623dee7161412fa29
[HannesMatthias] <[mh14270@gmail.com]>
2018-05-22 11:03:54 +0200

    login

--- StackSolvingProblems/index.php
+++ StackSolvingProblems/index.php
@@ -1,10 +1,10 @@
 <?php
 
-
+require_once 'model/entities/user.php';
 require_once 'model/entities/DB.php';
 require_once 'controller/Controller.php';
 
-$aktion = isset($_GET['aktion']) ? $_GET['aktion'] : 'menu';
+$aktion = isset($_GET['aktion']) ? $_GET['aktion'] : 'idea/idea';
 $controller = new Controller();
 
 if (method_exists($controller, $aktion)) {
