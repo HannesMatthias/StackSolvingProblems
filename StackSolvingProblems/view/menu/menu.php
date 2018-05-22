b20428fe348e5628cac93f5a99b92bbe06bf0e98
[HannesMatthias] <[mh14270@gmail.com]>
2018-05-22 10:04:51 +0200

    menu

---
+++ StackSolvingProblems/view/menu/menu.php
@@ -0,0 +1,15 @@
+<nav id="menu">
+    <ul>
+        <li><a href="index.php"><img id="logo" src="../images/logo.png" /></a></li>
+        <li>
+            <form>
+                <input type="search" id="searchbar" placeholder="Suche nach Fragen" />
+                <input type="submit" id="searchButton" value="Suchen" />
+
+            </form>
+        </li>
+        <li><a href="#">Frage stellen</a></li>
+        <li><a href="#">Probleme lösen</a> </li>        
+        <li><a href="#">An Projekten teilnehmen</a></li>
+    </ul>
+</nav>
