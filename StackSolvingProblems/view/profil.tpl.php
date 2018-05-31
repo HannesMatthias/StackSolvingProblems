66bbc87e783f0c9df9bd84a7f414942a0ca810c2
SFS01 <SFS01@users.noreply.github.com>
2018-05-31 13:22:45 +0200

    meh

--- StackSolvingProblems/view/profil.tpl.php
+++ StackSolvingProblems/view/profil.tpl.php
@@ -2,17 +2,20 @@
 <html lang="de">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="styles/profil.css">
+        <link rel="stylesheet" type="text/css" href="view/styles/profil.css">
         
     </head>
     <body>
         <div id="profil">
-            <h1>Profil</h1>
             <div id="profil_image">
                 <img src="view/images/lauch.jpg" alt="Smiley face" height="42" width="42">
             </div>
-            <div id="userdata"></div>
-            <div id="userquestions"></div>            
+            <div id="userdata">
+
+            </div>
+            <div id="userquestions">
+                
+            </div>            
         </div>
     </body>
 </html>
