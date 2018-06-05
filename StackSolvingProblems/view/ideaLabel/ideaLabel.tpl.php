99d1f6e84224692ffaf03df98cf2c55aa3c47540
ManuelCeschini <Manuel.Ceschini@gmail.com>
2018-06-05 11:35:48 +0200

    null

--- StackSolvingProblems/view/ideaLabel/ideaLabel.tpl.php
+++ StackSolvingProblems/view/ideaLabel/ideaLabel.tpl.php
@@ -1,19 +1,26 @@
 <div class="scollView">
 <?php for ($i = 1; $i <= 10; $i++){ ?>
-    <div class="ideaLabel">
+    <div class="ideaLabel" align="center">
         <table>
         <tr>
-            <th class="username"><label>username</label></th>  
-            <th class="profile"><label>profile</label></th>          
+            <tr>
+            <th class="profile"><label>profile</label></th>
+            <th class="username"><label>username</label></th> 
             <th class="status"><label>status</label></th>
+        </tr>
+            <tr>     
+            <th class="likesAndDislikesText"><label>like</label></th>     
+            <th class="description" ><label>description</label></th>
+            
+            
             <th class="likesAndDislikes">
                 <table>
-                    <tr><th><button type="button" class="buttonUp"><img src="view/images/up.png" width="15"></button></th></tr>
-                    <tr><th><button type="button" class="buttonDown"><img src="view/images/down.png" width="15"></button></th></tr>
+                    <th><button type="button" class="buttonUp"><img src="view/images/like.png" width="25"></button></th>
+                    <th><button type="button" class="buttonDown"><img src="view/images/dislike.png" width="25"></button></th>
                 </table>
             </th>
-            <th class="likesAndDislikesText" align="center"><label>like</label></th>
-        </tr>
+            
+        </tr></tr>
         </table>
     </div> 
     <?php } ?>
