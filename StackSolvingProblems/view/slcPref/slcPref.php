7bc2ff9119cd48fd11b4c295e267f7ca3a4afa8a
ManuelCeschini <Manuel.Ceschini@gmail.com>
2018-06-06 09:27:27 +0200

    a

--- StackSolvingProblems/view/slcPref/slcPref.php
+++ StackSolvingProblems/view/slcPref/slcPref.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
+
+ <!-- Hier hannes -->
     <link rel="stylesheet" type="text/css" href="slcPref.css">
-</head>
+
 <body>
     <div id="container">
         <div id="forum"><button type="button" id="forumButton"><img src="../images/forumIntro.png" width="400"></button></div>
         <div id="projects"><button type="button" id="forumButton"><img src="../images/ideaIntro.png" width="400"></button></div>
     </div>
-</body>
-</html>
