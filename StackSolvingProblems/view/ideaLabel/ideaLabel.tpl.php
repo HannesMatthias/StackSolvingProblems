62385a5a1d2c91b60c8429618132e47228e98f72
ManuelCeschini <Manuel.Ceschini@gmail.com>
2018-06-06 14:21:35 +0200

    .

--- StackSolvingProblems/view/ideaLabel/ideaLabel.tpl.php
+++ StackSolvingProblems/view/ideaLabel/ideaLabel.tpl.php
@@ -2,27 +2,21 @@
 <?php foreach($ideas as $i) {
     $user = Project::findUser($i->getUser_id()); ?>
     <div class="ideaLabel" align="center">
-        <table>
-        <tr>
-            <tr>
-            <td class="profile"><label><img src="<?php echo $user->getIcon(); ?>" alt="<?php echo $user->getUsername(); ?>_ProfilePic" height="42" width="42"></label></td>
-            <td class="title"><label><?php echo $i->getTitle(); ?></label></td> 
-            <td class="status"><label><?php echo $i->getStatus(); ?></label></td>
-        </tr>
-            <tr>     
-            <td class="username"><label><?php echo $user->getUsername(); ?></label></td>     
-            <td class="description" ><label><?php echo $i->getDescription(); ?></label></td>
-            
-            
-            <td class="likesAndDislikes">
-                <table>
-                    <td><button type="button" class="buttonUp"><img src="view/images/like.png" width="25"></button></td>
-                    <td><button type="button" class="buttonDown"><img src="view/images/dislike.png" width="25"></button></td>
-                </table>
-            </td>
-            
-        </tr></tr>
-        </table>
+        
+        <div id="container0">
+            <div class="profile"><label><img src="<?php echo $user->getIcon(); ?>" alt="<?php echo $user->getUsername(); ?>_ProfilePic" height="42" width="42"></label></div>
+            <div class="title"><label><?php echo $i->getTitle(); ?></label></div>
+            <div class="status"><label><?php echo $i->getStatus(); ?> test</label></div>
+        </div>
+        
+        <div id="container1">    
+            <div class="username"><label><?php echo $user->getUsername(); ?></label></div>
+            <div class="description" ><label><?php echo $i->getDescription(); ?></label></div>
+            <div class="likesAndDislikes">
+                    <div button type="button" class="buttonUp"><img src="view/images/like.png" width="25"></button></div>
+                    <div button type="button" class="buttonDown"><img src="view/images/dislike.png" width="25"></button></div>
+            </div>
+        </div>
     </div> 
     <?php } ?>
 </div>   
