1db6e928194fb73ea69c24640e54fabec97780b2
juergengoeller <juergengoeller2@gmail.com>
2018-06-06 13:39:11 +0200

    Refactor

--- StackSolvingProblems/view/ideaLabel/ideaLabel.tpl.php
+++ StackSolvingProblems/view/ideaLabel/ideaLabel.tpl.php
@@ -5,21 +5,22 @@
         <table>
         <tr>
             <tr>
-            <th class="profile"><label><img src="<?php $user->getProfile() ?>" alt="<? $user->getUsername() ?>_ProfilePic" height="42" width="42"></label></th>
-            <th class="title"><label><?php echo $i->getTitle(); ?></label></th> 
-            <th class="status"><label><?php echo  $i->getStatus(); ?></label></th>
+            <?php echo $user->getIcon(); ?> 
+            <td class="profile"><label><img src="<?php echo $user->getIcon(); ?>" alt="<?php echo $user->getUsername(); ?>_ProfilePic" height="42" width="42"></label></td>
+            <td class="title"><label><?php echo $i->getTitle(); ?></label></td> 
+            <td class="status"><label><?php echo $i->getStatus(); ?></label></td>
         </tr>
             <tr>     
-            <th class="username"><label><?php echo $user->getUsername(); ?></label></th>     
-            <th class="description" ><label><?php echo  $i->getDescription(); ?></label></th>
+            <td class="username"><label><?php echo $user->getUsername(); ?></label></td>     
+            <td class="description" ><label><?php echo $i->getDescription(); ?></label></td>
             
             
-            <th class="likesAndDislikes">
+            <td class="likesAndDislikes">
                 <table>
-                    <th><button type="button" class="buttonUp"><img src="view/images/like.png" width="25"></button></th>
-                    <th><button type="button" class="buttonDown"><img src="view/images/dislike.png" width="25"></button></th>
+                    <td><button type="button" class="buttonUp"><img src="view/images/like.png" width="25"></button></td>
+                    <td><button type="button" class="buttonDown"><img src="view/images/dislike.png" width="25"></button></td>
                 </table>
-            </th>
+            </td>
             
         </tr></tr>
         </table>
