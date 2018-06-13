e71ff2f1af2a9d25676f2bc7f7ef1880c33ae63d
Hannes <Hannes@ErazerX510.fritz.box>
2018-06-13 14:42:28 +0200

    .

--- StackSolvingProblems/model/entities/question.php
+++ StackSolvingProblems/model/entities/question.php
@@ -8,7 +8,7 @@
         private $dislikes = '';
         private $user_id = 0;
         private $solved = false;
-        private $right_answer = "sdsd";
+        private $right_answer = "";
         
     
  
