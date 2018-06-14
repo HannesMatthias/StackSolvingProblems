0f4d7c0d30c6e468a2072eda5d8317bba5249887
SFS01 <36699529+SFS01@users.noreply.github.com>
2018-06-14 10:24:05 +0200

    meh

--- StackSolvingProblems/view/editProfil.tpl.php
+++ StackSolvingProblems/view/editProfil.tpl.php
@@ -13,32 +13,34 @@
             <div id="username"><?php echo $user->getUsername(); ?></div>
             <div id="userdata">
                 <form action="index.php?action=saveProfil" method="POST" enctype="multipart/form-data">
-                    <div class="outer_left">
-                        <div class="inner_left">
-                            Vorname:
-                            Geburtsdatum:
-                            Email:
-                            Profilbild:
+                    <div id="form">
+                        <div class="outer_left">
+                            <div class="inner_left">
+                                <div class="tag">Vorname:</div>
+                                <div class="tag">Geburtsdatum:</div>
+                                <div class="tag">Email:</div>
+                            </div>
+                            <div class="inner_right">
+                                <div class="tag"><input type="text" name="name" value="<?php echo $user->getName(); ?>"></div>
+                                <div class="tag"><input type="date" name="birthdate" max="<?php echo date("Y-m-d") ?>" value="<?php echo $user->getBirthdate(); ?>"></div>
+                                <div class="tag"><input type="email" name="email" value="<?php echo $user->getEmail(); ?>"></div>
+                            </div>
                         </div>
-                        <div class="inner_right">
-                            <input type="text" name="name" value="<?php echo $user->getName(); ?>">
-                            <input type="date" name="birthdate" max="<?php echo date("Y-m-d") ?>" value="<?php echo $user->getBirthdate(); ?>">
-                            <input type="email" name="email" value="<?php echo $user->getEmail(); ?>">
-                            <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*">
+                        <div class="outer_right">
+                            <div class="inner_left">
+                                <div class="tag">Nachname:</div>
+                                <div class="tag">Geschlecht:</div>
+                                <div class="tag">Profilbild:</div>
+                            </div>
+                            <div class="inner_right">
+                                <div class="tag"><input type="text" name="surname" value="<?php echo $user->getSurname(); ?>"></tag>
+                                <div class="tag"><input type="radio" name="sex" value="m" <?php if ($user->getSex() == "m") echo "checked";?>> Männlich
+                                <input type="radio" name="sex" value="w" <?php if ($user->getSex() == "w") echo "checked";?>> Weiblich</div>
+                                <div class="tag"><input type="file" name="fileToUpload" id="fileToUpload" accept="image/*"></div>
+                            </div>
                         </div>
                     </div>
-                    <div class="outer_right">
-                        <div class="inner_left">
-                            Nachname:
-                            Geschlecht:
-                        </div>
-                        <div class="inner_right">
-                            <input type="text" name="surname" value="<?php echo $user->getSurname(); ?>">
-                            <input type="radio" name="sex" value="m" <?php if ($user->getSex() == "m") echo "checked";?>> Männlich
-                            <input type="radio" name="sex" value="w" <?php if ($user->getSex() == "w") echo "checked";?>> Weiblich
-                        </div>
-                    </div>
-                    <input type="submit" name="submit" value="Ändern">
+                    <input id="submit" type="submit" name="submit" value="Ändern">
                 </form>
             </div>
         </div>
